3bab9f91c5261a563ca9f112e4fcb16c29ecb112
Elymar Apao <jozzua@gmail.com>
2013-03-25 15:47:34 +0800

    Improved Parallax version

--- app.rb
+++ app.rb
@@ -4,21 +4,6 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'slim'
 
-
-helpers do
-    def username
-          session[:identity] ? session[:identity] : 'Hello stranger'
-            end
-end
-
-before '/secure/*' do
-    if !session[:identity] then
-          session[:previous_url] = request.path
-              @error = 'Sorry guacamole, you need to be logged in to visit ' + request.path
-                  halt erb(:login_form)
-                    end
-end
-
 get '/' do
     File.read(File.join('public', 'index.html'))
 end
